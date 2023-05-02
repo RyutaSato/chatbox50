d503771d99de9102f0e6d58d9f7fa0cf54fd1761
rsato <sato.ryuta.main@gmail.com>
2023-05-02 11:26:52 +0900

    :hammer: added ServiceWorker

--- chatbox50/__init__.py
+++ chatbox50/__init__.py
@@ -2,3 +2,4 @@ from chatbox50.message import Message, SentBy
 from chatbox50.chat_client import ChatClient
 from chatbox50.db_session import SQLSession
 from chatbox50.chatbox import Chatbox
+from chatbox50.service_worker import ServiceWorker
