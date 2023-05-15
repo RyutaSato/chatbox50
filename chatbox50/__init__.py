76534d342eeed421be5ac7828e3431f20819e890
rsato <sato.ryuta.main@gmail.com>
2023-05-16 02:19:47 +0900

    :art: renamed `ChatClient` to `Connection`

--- chatbox50/__init__.py
+++ chatbox50/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from chatbox50.message import Message, SentBy
-from chatbox50.chat_client import ChatClient
+from chatbox50.connection import Connection
 from chatbox50.db_session import SQLSession
 from chatbox50.chatbox import ChatBox
 from chatbox50.service_worker import ServiceWorker
